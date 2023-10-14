979b6ed310ba61244675db3d7317d5d6d2f001c9
Guilherme Ferreira <guilherme@MacBook-Air-de-Guilherme.local>
2023-10-14 11:19:56 -0300

    task service, environments files

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+
+  private apiUrl = environment.apiUrl;
 
   constructor() { }
 }
