ab2bc65f04c531200e7acd9e0c6a4ccdcd036420
Guilherme Ferreira <guilherme@MacBook-Air-de-Guilherme.local>
2023-10-14 01:30:32 -0300

    dashboard, routing, adjust in nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,14 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from './components/template/footer/footer.component';
+import { DashboardComponent } from './components/view/dashboard/dashboard.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
