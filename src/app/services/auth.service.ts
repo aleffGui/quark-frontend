9adeaa39247a0a5c99c76bf8fa51e7c626cf5c14
Guilherme <alexguilhermeferreiraa@gmail.com>
2023-10-24 13:07:10 -0300

    adjusts deploy

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { UserCredentials } from '../models/dtos/user.credentials';
 import { TokenService } from './token.service';
+import { environment } from '../config';
 
 @Injectable({
   providedIn: 'root'
